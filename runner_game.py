43dd9c514ee8ac25f34c4c6f3c72808783c2b9ad
TheADiSSY <tisgot23adioka@innovationsgymnasiet>
2024-04-09 16:03:01 +0200

    Added actual code to the project

--- runner_game.py
+++ runner_game.py
@@ -2,3 +2,32 @@
 import pygame
 import time
 import random
+import cv2
+from PIL import Image, ImageTk
+from tkinter import Label, PhotoImage, Tk
+
+
+#Name, background and CLOCK
+pygame.display.set_caption("Rooftop Run")
+
+Screen_Height = 1080
+Screen_Width = 1920
+Screen = pygame.display.set_mode((Screen_Width, Screen_Height))
+
+pygame.init()
+background = 'C:/Desktop/Runner Game/runner bg.png'
+clock = pygame.time.Clock()
+
+#The gameloop
+running = True
+while running:
+    #The game's background
+    Screen.blit(background, (0, 0))
+    
+    # Update the display
+    pygame.display.flip()
+
+    #Closing the game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
